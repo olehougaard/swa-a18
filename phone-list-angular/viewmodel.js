e11b3f0f8dc529a352fa267295ec876b6db23a1d
= <=>
2018-10-08 06:01:04 +0200

    Initial commit

---
+++ phone-list-angular/viewmodel.js
@@ -0,0 +1,15 @@
+function viewModel(model) {
+    let filter = ''
+    let phones = model.phones
+
+    function updateFilter(_filter) {
+        filter = _filter.toLowerCase()
+        phones = model.phones.filter(ph => ph.name().toLowerCase().includes(filter))
+    }
+
+    const vm = { }
+    Object.defineProperty(vm, 'filter', { get: () => filter, set: updateFilter })
+    Object.defineProperty(vm, 'phones', { get: () => phones, set: () => {} })
+
+    return vm
+}
