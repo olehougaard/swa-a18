b72b91ad400434be2234aa30813445a34d8f4a47
= <=>
2018-10-08 06:07:49 +0200

    Removed the use of components for illistrative purposes

--- phone-list-angular/components.js
+++ phone-list-angular/components.js
@@ -1,22 +1,4 @@
 const module = angular.module('phoneApp', [])
-module.value('$model', viewModel(model()))
-module.component('modelInput', {
-    bindings: { propertyName: '@' },
-    template: '<input ng-model="$ctrl.model[$ctrl.propertyName]">',
-    controller: ['$model', function ($model) {
-        this.model = $model
-    }]            
-})
-module.component('phoneTable', {
-    template:   '<table>' +
-                    '<tr ng-repeat="phone in $ctrl.model.phones">' +
-                        '<td>{{phone.name()}}</td><td>{{phone.snippet()}}</td>' +
-                    '</tr>' +
-                '<table>',
-    controller: ['$model', function ($model) {
-        this.model = $model
-    }]
-})
-module.controller('PhoneController', function($scope, $model) {
-    $scope.model = $model
+module.controller('PhoneController', function($scope) {
+    $scope.model = viewModel(model())
 })
