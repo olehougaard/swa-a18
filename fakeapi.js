9b7a48ec7140e1b5c9feb78ccdff1a2693c52736
Ole Ildsgaard Hougaard <olehougaard@gmail.com>
2018-10-28 09:53:16 +0100

    Upgraded the fakeapi to better showcase promises

--- fakeapi.js
+++ fakeapi.js
@@ -2,88 +2,59 @@ const callback = {
     fetchPerson(id, callback, error) {
         switch(id) {
             case 1: 
-                if (callback) callback({ id: 1, name: 'John Doe', employee: false })
+                if (callback) setImmediate(() => callback({ id: 1, name: 'John Doe' }))
                 break
             case 2: 
-                if (callback) callback({ id: 2, name: 'Jane Doe', employee: true, manager: false })
+                if (callback) setImmediate(() => callback({ id: 2, name: 'Jane Doe', employeeId: 17 }))
                 break
             case 3: 
-                if (callback) callback({ id: 3, name: 'George Deer', employee: true, manager: false })
+                if (callback) setImmediate(() => callback({ id: 3, name: 'George Deer', employee: true, manager: false }))
                 break
             case 4: 
-                if (callback) callback({ id: 4, name: 'Jill Deer', employee: true, manager: true })
+                if (callback) setImmediate(() => callback({ id: 4, name: 'Jill Deer', employee: true, manager: true }))
                 break
             default: 
-                if (error) error('nope')
+                if (error) setImmediate(() => error('nope'))
         }
     },
 
-    fetchSalary(id, callback, error) {
+    fetchEmployee(id, callback, error) {
         switch(id) {
-            case 2: 
-                if (callback) callback(42000)
+            case 17: 
+                if (callback) setImmediate(() => callback({ employeeId: 17, salary: 42000, manager: false}))
                 break
-            case 3: 
-                if (callback) callback(35000)
+            case 19: 
+                if (callback) setImmediate(() => callback({ employeeId: 19, salary: 35000, manager: false }))
                 break
-            case 4: 
-                if (callback) callback(74000)
+            case 23: 
+                if (callback) setImmediate(() => callback({ employeeId: 23, salary: 74000, manager: true }))
                 break
             default: 
-                if (error) error('nope')
+                if (error) setImmediate(() => error('nope'))
         }
     },
 
-    fetchAllSalaries(callback) {
-        if (callback)
-            callback({
-                '2': 42000,
-                '3': 35000,
-                '4': 74000
-            })
-    },
-
-    fetchEmployees(id, callback, error) {
+    fetchSubordinates(id, callback, error) {
         if (id === 4 && callback) 
-            callback([2, 3])
+            setImmediate(() => callback([2, 3]))
         else if (error)
-            error('nope')
+            setImmediate(() => error('nope'))
     }
 
 }
 
-const promise = {
-    fetchPerson(id) {
-        switch(id) {
-            case 1: return Promise.resolve({ id: 1, name: 'John Doe', employee: false })
-            case 2: return Promise.resolve({ id: 2, name: 'Jane Doe', employee: true, manager: false })
-            case 3: return Promise.resolve({ id: 3, name: 'George Deer', employee: true, manager: false })
-            case 4: return Promise.resolve({ id: 4, name: 'Jill Deer', employee: true, manager: true })
-            default: return Promise.reject('nope')
-        }
-    },
-
-    fetchSalary(id) {
-        switch(id) {
-            case 2: return Promise.resolve(42000)
-            case 3: return Promise.resolve(35000)
-            case 4: return Promise.resolve(74000)
-        default: return Promise.reject('nope')
-        }
-    },
-
-    fetchAllSalaries() {
-        return Promise.resolve({
-            '2': 42000,
-            '3': 35000,
-            '4': 74000
-        })
-    },
-
-    fetchEmployees(id) {
-        if (id === 4) return Promise.resolve([2, 3])
-        return Promise.reject('nope')
+const createPromise = (callbackMethod) => (...args) => new Promise(
+    (resolve, reject) => {
+        const allArgs = [...args, resolve, reject]
+        callbackMethod(...allArgs)
     }
+)
+
+const promise = {
+    fetchPerson: createPromise(callback.fetchPerson),
+    fetchEmployee: createPromise(callback.fetchEmployee),
+    fetchAllSalaries: createPromise(callback.fetchAllSalaries),
+    fetchEmployees: createPromise(callback.fetchAllSalaries)
 }
 
 module.exports = { callback, promise }
